6a27492ff285e539399b5703d6093273fa444993
caetanovini <viniciusnandez@gmail.com>
2026-03-25 19:42:11 +0000

    feat: add config file

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,11 +1,19 @@
+{{
+    config(
+        materialized = 'incremental',
+        unique_key = 'order_id',
+        incremental_strategy = 'merge'
+    )
+}}
+
 with orders as  (
-    select * from {{ ref ('stg_jaffle_shop__orders' )}}
+    select * 
+    from {{ ref ('stg_jaffle_shop__orders' )}}
 ),
-
 payments as (
-    select * from {{ ref ('stg_stripe__payments') }}
+    select * 
+    from {{ ref ('stg_stripe__payments') }}
 ),
-
 order_payments as (
     select
         order_id,
@@ -14,17 +22,17 @@ order_payments as (
     from payments
     group by 1
 ),
-
  final as (
-
     select
         orders.order_id,
         orders.customer_id,
         orders.order_date,
         coalesce (order_payments.amount, 0) as amount
-
     from orders
     left join order_payments using (order_id)
 )
-
-select * from final
+select * 
+from final
+{% if is_incremental() %}
+    where order_date >= (select max(order_date) from {{this}})
+{% endif %}
