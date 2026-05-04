34f72dcfb66e732d35657467d3cee2e898782b99
caetanovini <viniciusnandez@gmail.com>
2026-05-04 20:25:17 +0100

    feat: update files

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -6,5 +6,10 @@ select
         when status not in ('returned','return_pending') 
         then order_date 
     end as valid_order_date,
-    status
+    case
+        when status like '%shipped%' then 'shipped'
+        when status like '%return%' then 'returned'
+        when status like '%pending%' then 'placed'
+        else status 
+    end as status
 from {{ source('jaffle_shop', 'orders') }}
